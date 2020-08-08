f4f804e219c852aea3cf83c4cbcf178bcebc9e88
Harrhy Saladagu <harrhys@gmail.com>
2020-08-08 23:22:27 +0530

    updated db url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://harrhys:Janu1234$@confusiondb.hlybn.mongodb.net/confusiondb?retryWrites=true&w=majority'
+    'mongoUrl' : 'mongodb+srv://harrhys:janu1234@confusiondb.hlybn.mongodb.net/confusiondb'
 }
